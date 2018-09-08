90e5edb2011cd7df0413be9163e19bbb700d7dc3
Michael Mudge <michaeltmudge@gmail.com>
2018-09-08 09:59:59 +0200

    installed jquery ui, sortable html5 library, and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery-ui
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require activestorage
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
