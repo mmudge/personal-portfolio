55bc67c774b1ab3580804c10c1fc3820f0f3a886
michael mudge <michaeltmudge@gmail.com>
2019-01-16 15:28:23 +0100

    updated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://mm-personal-portfolio.herokuapp.com', 'http://mm-personal-portfolio.herokuapp.com', 'www.michaelmudge.com', 'michaelmudge.com']
+  config.action_cable.url = "wss://mm-personal-portfolio.herokuapp.com/cable"
 end
