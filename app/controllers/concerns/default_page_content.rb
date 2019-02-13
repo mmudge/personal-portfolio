5f42b62f1d7500a0cbb27bad45fa3f754b39a7e6
michael mudge <michaeltmudge@gmail.com>
2019-02-13 15:33:02 +0100

    updated seo keywords

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,6 +7,6 @@ module DefaultPageContent
 
   def set_page_defaults
     @page_title = "Michael Mudge | Portfolio"
-    @seo_keywords = "Michael Mudge's portfolio"
+    @seo_keywords = ["Web Development", "Software Development", "JavaScript", "Developer", "Software Engineer", "Michael Mudge"]
   end
 end
